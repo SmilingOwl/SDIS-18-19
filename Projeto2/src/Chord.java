a3e8f72793eda5b985328d55fbb1fbb43e1cb507
Juliana Marques <julianamcm05@gmail.com>
2019-05-16 17:48:27 +0100

    ..

--- Projeto2/src/Chord.java
+++ Projeto2/src/Chord.java
@@ -10,21 +10,31 @@ public class Chord {
     public Chord(PeerInfo owner) {
         this.owner = owner;
         this.predecessor = owner;
+<<<<<<< Updated upstream
         this.fingerTable = new PeerInfo[NUM_FINGERS];
         for(int i = 0; i < this.fingerTable.length; i++) {
             this.fingerTable[i] = owner;
+=======
+        this.fingers = new PeerInfo[NUM_FINGERS];
+        
+        for(int i = 0; i < fingers.length; i++) {
+            this.fingers[i] = owner;
+>>>>>>> Stashed changes
         }
     }
 
     /************************** Setters **************************/
+   
     public void set_predecessor(PeerInfo predecessor) {
         this.predecessor = predecessor;
     }
+
 
     /************************** Others **************************/
     public void update_fingers(PeerInfo new_peer) {
         System.out.println("entered");
         BigInteger new_peer_id = new_peer.get_id();
+        
         //a node with new_peer_id joined
         for(int i = 0; i < this.fingerTable.length; i++) {
             int value = (1 + (int)Math.pow(2, i)) % 160;
